f7e9daafeda454375adc1a0b82c6ff74716b7fd1
boba2fett <boba2002fett@gmail.com>
2020-12-29 18:14:22 +0100

    whacky

--- build.sh
+++ build.sh
@@ -8,5 +8,6 @@ pip3 install speechrecognition
 pip3 install pocketsphinx
 pip3 install pyttsx3
 pip3 install colorama
+mv custom_commands/mycommands.py.template custom_commands/mycommands.py
 mv lib/python3.9/site-packages/speech_recognition/pocketsphinx-data/en-US/pronounciation-dictionary.dict lib/python3.9/site-packages/speech_recognition/pocketsphinx-data/en-US/pronounciation-dictionary.dict.backup
 python3 build.py
